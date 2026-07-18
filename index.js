ff72bd7b5b53dd54d130296b27013c76b19a750c
ajayprajapati6391 <ajay951738kk@gmail.com>
2026-07-18 13:45:42 +0530

    Initial commit

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "https://emp-frontend-b8vf.vercel.app/"],
     credentials: true,
+     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
 );
 
