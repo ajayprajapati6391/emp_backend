2b1115e2a655b9c2a5b19991751cd08858ec133c
ajayprajapati6391 <ajay951738kk@gmail.com>
2026-07-18 12:47:44 +0530

    first commit

--- Config/db.js
+++ Config/db.js
@@ -1,19 +1,13 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    if (mongoose.connection.readyState >= 1) {
-      return;
-    }
-
     await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB Connected");
   } catch (err) {
-    console.log("MongoDB Error:", err.message);
+    console.error("MongoDB Connection Error:", err);
+    process.exit(1);
   }
 };
 
