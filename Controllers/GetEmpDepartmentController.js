362e1defcdc540c3fa5103ae3310bfe4feddcaad
ajayprajapati6391 <ajay951738kk@gmail.com>
2026-07-22 16:42:29 +0530

    change

--- Controllers/GetEmpDepartmentController.js
+++ Controllers/GetEmpDepartmentController.js
@@ -3,42 +3,43 @@ import EmployeeModel from "../Models/EmployeeModel.js";
 const getEmployeesByDepartment = async (req, res) => {
   try {
     const { department } = req.params;
-    let { page, limit } = req.query;
+    // let { page, limit } = req.query;
+const employees = await EmployeeModel.find({department:department});
+res.send(employees);
+    // page = parseInt(page) || 1;
+    // limit = parseInt(limit) || 5;
 
-    page = parseInt(page) || 1;
-    limit = parseInt(limit) || 5;
+    // const skip = (page - 1) * limit;
 
-    const skip = (page - 1) * limit;
+    // const searchCriteria = {
+    //   department: {
+    //     $regex: `^${department}$`,
+    //     $options: "i",
+    //   },
+    // };
 
-    const searchCriteria = {
-      department: {
-        $regex: `^${department}$`,
-        $options: "i",
-      },
-    };
+    // const totalEmployee = await EmployeeModel.countDocuments(searchCriteria);
 
-    const totalEmployee = await EmployeeModel.countDocuments(searchCriteria);
+    // const employees = await EmployeeModel.find(searchCriteria)
+    //   .skip(skip)
+    //   .limit(limit)
+    //   .sort({ updatedAt: -1 });
 
-    const employees = await EmployeeModel.find(searchCriteria)
-      .skip(skip)
-      .limit(limit)
-      .sort({ updatedAt: -1 });
+    // const totalPage = Math.ceil(totalEmployee / limit);
 
-    const totalPage = Math.ceil(totalEmployee / limit);
-
-    res.status(200).json({
-      success: true,
-      message: "Department employees fetched successfully",
-      data: {
-        employees,
-        pagination: {
-          totalEmployee,
-          totalPage,
-          currentPage: page,
-          pageSize: limit,
-        },
-      },
-    });
+    // res.status(200).json({
+    //   success: true,
+    //   message: "Department employees fetched successfully",
+    //   data: {
+    //     employees,
+    //     pagination: {
+    //       totalEmployee,
+    //       totalPage,
+    //       currentPage: page,
+    //       pageSize: limit,
+    //     },
+    //   },
+    // });
   } catch (err) {
     console.log(err);
     res.status(500).json({
